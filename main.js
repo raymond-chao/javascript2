251310a0bf999ef536d75b6a017cfcf65d73ee1e
raymond-chao <raymond_chao45@hotmail.com>
2025-03-23 21:16:15 +0100

    js changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let price = prompt("Enter the price of the product: ");
-let removeDollarSign = price.slice(1).trim();
+let removeDollarSign = price.slice(1);
 let discount = 0.9;
 let newPrice = parseFloat(removeDollarSign) * discount;
 console.log(`Your new price is: $${newPrice}`);
